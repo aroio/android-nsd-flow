38e0e185ac433b3c0e92d121b359e03ef2a9acd7
LennartEgb <lennartegbers@hotmail.com>
2021-02-06 22:52:20 +0100

    Update ResolveListenerFlow.kt
    * close channel on failure
    * sendBlocking on success

--- android-nsd-flow/src/main/java/de/aroio/library/nsd/flow/resolve/ResolveListenerFlow.kt
+++ android-nsd-flow/src/main/java/de/aroio/library/nsd/flow/resolve/ResolveListenerFlow.kt
@@ -4,16 +4,17 @@ import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.channels.sendBlocking
 import java.util.concurrent.CancellationException
 
 internal class ResolveListenerFlow(
         private val producerScope: ProducerScope<ResolveEvent>
 ) : NsdManager.ResolveListener {
     override fun onResolveFailed(nsdServiceInfo: NsdServiceInfo, errorCode: Int) {
-        producerScope.cancel(CancellationException("Resolving failed with errorCode $errorCode for serviceInfo $nsdServiceInfo"))
+        producerScope.channel.close()
     }
 
     override fun onServiceResolved(nsdServiceInfo: NsdServiceInfo) {
-        producerScope.offer(ResolveEvent.ServiceResolved(nsdServiceInfo))
+        producerScope.sendBlocking(ResolveEvent.ServiceResolved(nsdServiceInfo))
     }
 }
