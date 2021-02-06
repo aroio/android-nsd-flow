a67ba62524b574b37c2e8429347ed316bc888a00
LennartEgb <lennartegbers@hotmail.com>
2021-02-06 22:51:27 +0100

    Update RegistrationListenerFlow.kt
    * close channel on failure

--- android-nsd-flow/src/main/java/de/aroio/library/nsd/flow/registration/RegistrationListenerFlow.kt
+++ android-nsd-flow/src/main/java/de/aroio/library/nsd/flow/registration/RegistrationListenerFlow.kt
@@ -4,25 +4,26 @@ import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.channels.sendBlocking
 import java.util.concurrent.CancellationException
 
 internal class RegistrationListenerFlow(
         private val producerScope: ProducerScope<RegistrationEvent>
 ) : NsdManager.RegistrationListener {
     override fun onRegistrationFailed(nsdServiceInfo: NsdServiceInfo, errorCode: Int) {
-        producerScope.cancel(CancellationException("Registration failed with errorCode $errorCode for NsdServiceInfo $nsdServiceInfo"))
+        producerScope.channel.close()
     }
 
     override fun onUnregistrationFailed(nsdServiceInfo: NsdServiceInfo, errorCode: Int) {
-        producerScope.cancel(CancellationException("Unegistration failed with errorCode $errorCode for NsdServiceInfo $nsdServiceInfo"))
+        producerScope.channel.close()
     }
 
     override fun onServiceRegistered(nsdServiceInfo: NsdServiceInfo) {
-        producerScope.offer(RegistrationEvent.ServiceRegistered(nsdServiceInfo))
+        producerScope.sendBlocking(RegistrationEvent.ServiceRegistered(nsdServiceInfo))
     }
 
     override fun onServiceUnregistered(nsdServiceInfo: NsdServiceInfo) {
-        producerScope.offer(RegistrationEvent.ServiceUnregistered(nsdServiceInfo))
+        producerScope.sendBlocking(RegistrationEvent.ServiceUnregistered(nsdServiceInfo))
     }
 
 }
