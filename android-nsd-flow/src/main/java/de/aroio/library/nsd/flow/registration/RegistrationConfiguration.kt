2900742a799d19e784dc73a81ba48d9023ae0928
LennartEgb <lennartegbers@hotmail.com>
2021-02-18 22:09:21 +0100

    Update RegistrationConfiguration.kt
    * two ways of creating RegistrationConfiguration

--- android-nsd-flow/src/main/java/de/aroio/library/nsd/flow/registration/RegistrationConfiguration.kt
+++ android-nsd-flow/src/main/java/de/aroio/library/nsd/flow/registration/RegistrationConfiguration.kt
@@ -1,13 +1,26 @@
 package de.aroio.library.nsd.flow.registration
 
 import android.net.nsd.NsdManager
+import android.net.nsd.NsdServiceInfo
 import de.aroio.library.nsd.flow.ProtocolType
 
-data class RegistrationConfiguration(
-        val serviceName: String = "default",
-        val serviceType: String = "_http._tcp.",
-        val port: Int,
+fun RegistrationConfiguration(
+        serviceName: String = "default",
+        serviceType: String = "_http._tcp.",
+        port: Int,
+        @ProtocolType
+        protocolType: Int = NsdManager.PROTOCOL_DNS_SD
+) = RegistrationConfiguration(
+        nsdServiceInfo = NsdServiceInfo().apply {
+            setServiceName(serviceName)
+            setServiceType(serviceType)
+            setPort(port)
+        },
+        protocolType = protocolType
+)
 
+data class RegistrationConfiguration(
+        val nsdServiceInfo: NsdServiceInfo,
         @ProtocolType
         val protocolType: Int = NsdManager.PROTOCOL_DNS_SD
 )
